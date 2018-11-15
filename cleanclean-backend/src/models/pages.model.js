816e825c3bbdf91f5ab3b93a9dbe83b73cf48968
Jianfeng <cnjianfeng@gmail.com>
2018-11-15 11:39:15 +0800

    added create-org page

--- cleanclean-backend/src/models/pages.model.js
+++ cleanclean-backend/src/models/pages.model.js
@@ -6,7 +6,13 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
   const pages = new Schema({
-    text: { type: String, required: true }
+    name: { type: String, required: true },
+    action: { type: String, required: true },
+    user: {
+      oid: { type: Schema.Types.ObjectId },
+      data: { type: Schema.Types.Mixed }
+    },
+    data: { type: Schema.Types.Mixed }
   }, {
     timestamps: true
   });
